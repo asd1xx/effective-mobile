316b892e3a2d548f7e5e999a4045c908962d500f
asd2d <872122@mail.ru>
2025-05-17 15:00:33 +0300

    refactor: remove constant

--- Блок 1: Основы и работа с данными/3 Работа с массивами и коллекциями/task1.php
+++ Блок 1: Основы и работа с данными/3 Работа с массивами и коллекциями/task1.php
@@ -7,8 +7,8 @@ function filterEvenNumbers(array $numbers): array
     return array_filter($numbers, fn($number) => $number % 2 === 0);
 }
 
-print_r(filterEvenNumbers([1, 2, 3, 4, 5, 6])) . PHP_EOL;
+print_r(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 // ✅ Ожидаемый результат: [2, 4, 6]
 
-print_r(filterEvenNumbers([11, 15, 21])) . PHP_EOL;
+print_r(filterEvenNumbers([11, 15, 21]));
 // ✅ Ожидаемый результат: []
