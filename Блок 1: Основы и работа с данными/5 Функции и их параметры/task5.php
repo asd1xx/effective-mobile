5ed574a4fc06f5df41870aa79b8fa0ddeb4a2b14
asd2d <872122@mail.ru>
2025-05-18 15:07:43 +0300

    feat: implement a function generatePassword()

---
+++ Блок 1: Основы и работа с данными/5 Функции и их параметры/task5.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+function generatePassword(int $length = 8, bool $includeNumbers = true, bool $includeSpecialChars = false): string
+{
+    $numbers = '0123456789';
+    $specialChars = '$^.*+?\/{}[]()|';
+
+    if ($includeNumbers === true) {
+        $charsForPassword = $numbers;
+    }
+
+    if ($includeSpecialChars === true) {
+        $charsForPassword = $numbers . $specialChars;
+    }
+
+    $charsLength = mb_strlen($charsForPassword);
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $charsForPassword[mt_rand(0, $charsLength - 1)];
+    }
+
+    return $password;
+}
+
+echo generatePassword() . PHP_EOL;  
+// ✅ Должен быть 8-значный пароль с цифрами.
+
+echo generatePassword(length: 12, includeSpecialChars: true) . PHP_EOL;  
