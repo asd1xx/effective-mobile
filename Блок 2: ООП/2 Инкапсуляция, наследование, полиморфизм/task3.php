2f46561b2548417922fe61bfc153b578b27831e3
asd2d <872122@mail.ru>
2025-05-22 19:35:29 +0300

    feat: create class CreditAccount

---
+++ Блок 2: ООП/2 Инкапсуляция, наследование, полиморфизм/task3.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+class BankAccount
+{
+    public const int MIN_BALANCE = 0;
+
+    protected float $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->setBalance($balance);
+    }
+
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(float $value): void
+    {
+        $this->balance = $value;
+    }
+
+    public function deposit(float $value): void
+    {
+        $this->balance += $value;
+    }
+
+    public function withdraw(float $value): void
+    {
+        if ($this->balance - $value < self::MIN_BALANCE) {
+            print_r('Ошибка: недостаточно средств' . PHP_EOL);
+        }
+
+        $this->balance -= $value;
+    }
+}
+
+class CreditAccount extends BankAccount
+{
+    public const int MIN_CREDIT_BALANCE = -500;
+
+    public function withdraw(float $value): void
+    {
+        if ($this->balance - $value < self::MIN_CREDIT_BALANCE) {
+            print_r('Ошибка: допустимый минус превышен' . PHP_EOL);
+            return;
+        }
+
+        $this->balance -= $value;
+    }
+}
+
+$credit = new CreditAccount(1000);
+$credit->withdraw(1500);
+echo $credit->getBalance() . PHP_EOL;
+// ✅ -500 (допустимый минус)
