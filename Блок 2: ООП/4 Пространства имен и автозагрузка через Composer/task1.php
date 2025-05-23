de03a00aad1b397af3900e38e9d9156b778887f9
asd2d <872122@mail.ru>
2025-05-23 11:22:14 +0300

    feat: create class User

---
+++ Блок 2: ООП/4 Пространства имен и автозагрузка через Composer/task1.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+class User
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
+$user = new User("Иван");
+echo $user->getName() . PHP_EOL;
+// ✅ "Иван"
