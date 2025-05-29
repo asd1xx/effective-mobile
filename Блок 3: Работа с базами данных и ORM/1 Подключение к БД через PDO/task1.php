1ab0f6c6818bb133f4759e424e258463e87041c1
asd2d <872122@mail.ru>
2025-05-29 23:55:46 +0300

    refactor: add multiple connections

--- Блок 3: Работа с базами данных и ORM/1 Подключение к БД через PDO/task1.php
+++ Блок 3: Работа с базами данных и ORM/1 Подключение к БД через PDO/task1.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 final class Database
 {
-    public function connect()
+    public function connect(): void
     {
-        $host = getenv('HOST');
-        $dbname = getenv('DBNAME');
-        $username = getenv('USERNAME');
-        $password = getenv('PASSWORD');
+        // Параметры для подключения к БД
+        // $host = getenv('HOST');
+        // $dbname = getenv('DBNAME');
+        // $username = getenv('USERNAME');
+        // $password = getenv('PASSWORD');
 
         try {
-            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            // Строка подключения к БД MySql
+            // $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            // Строка подключения к БД PostgreSQL
+            // $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+            // Строка подключения к БД SQLite
+            $pdo = new PDO("sqlite:my_database.sqlite");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Подключение успешно!";
         } catch (PDOException $e) {
