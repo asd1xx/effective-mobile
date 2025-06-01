0d382645d66f75e86748d787fedec28166957ec0
asd2d <872122@mail.ru>
2025-06-01 23:25:23 +0300

    feat: add email validation

---
+++ Блок 3: Работа с базами данных и ORM/2 Защита от SQL-инъекций/task5.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+interface DatabaseInterface
+{
+    public function connect(): object;
+}
+
+final class Database implements DatabaseInterface
+{
+    public function connect(): object
+    {
+        try {
+            $pdo = new PDO("sqlite:my_database.sqlite");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Ошибка подключения: " . $e->getMessage());
+        }
+    }
+}
+
+class UserRepository
+{
+    private DatabaseInterface $db;
+
+    public function __construct(DatabaseInterface $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getUserByEmail(string $email): array|false
+    {
+        $validEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+        if (!$validEmail) {
+            die("Неверный формат email" . PHP_EOL);
+        }
+        
+        $pdo = $this->db->connect();
+
+        try {
+            $sql = "SELECT * FROM users WHERE email = :email";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                'email' => $validEmail,
+            ]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $user;
+        } catch (Exception $e) {
+            echo "Ошибка: " . $e->getMessage();
+        }
+    }
+}
+
+$db = new Database();
+$repo = new UserRepository($db);
+$repo->getUserByEmail("неправильный_адрес");
+// ✅ Должна быть ошибка "Неверный формат email"
