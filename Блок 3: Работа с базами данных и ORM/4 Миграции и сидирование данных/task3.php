1d6ad2a7624554585146a0c67e37a55b43df6adb
asd2d <872122@mail.ru>
2025-06-02 10:24:45 +0300

    feat: add a role column to the users table

---
+++ Блок 3: Работа с базами данных и ORM/4 Миграции и сидирование данных/task3.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+// Поставил уникальный неймспейс во избежании конфликтов с ранее созданными классами
+namespace Block3Migrations4Task3;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleToUsersTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user');
+        });
+    }
+    
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+}
+
+// php artisan migrate  
+// ✅ В таблице `users` появилось поле `role`
